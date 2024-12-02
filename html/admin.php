6af2550c918a04db804ced3959c2248770705486
NoahMiMa <noah.mirandamatos@gmail.com>
2024-12-02 09:52:10 +0100

    Changement page admin et secretaire

---
+++ html/admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page de connexion</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<header class="navbar">
+    <nav>
+        <a href="admin.php">Accueil</a>
+        <a href="Pre_admission.php">Pré-admission</a>
+    </nav>
+</header>
+<body>
+    <div class="container">
+    <div></div>  
+</body>
+</html>
---
+++ html/secretaire.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page de connexion</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<header class="navbar">
+    <nav>
+            <a href="secretaire.php">Accueil</a>
+            <a href="Pre_admission_Info.php">Pré-admission</a>
+            <a href="Site_clinique.php">Déconnexion</a>
+    </nav>
+</header>
+<body>
+    <div class="container">
+    <div></div>  
+</body>
+</html>
