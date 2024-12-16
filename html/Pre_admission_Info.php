aa14391a516880392910d79fa83169199717849b
NoahMiMa <noah.mirandamatos@gmail.com>
2024-12-16 08:53:46 +0100

    Test navbar

--- html/Pre_admission_Info.php
+++ html/Pre_admission_Info.php
@@ -79,16 +79,16 @@ error_reporting(E_ALL);
     <title>Pré admission</title>
     <link rel="stylesheet" href="style/style.css">
 </head>
-<header class="navbar">
-        <nav>
-            <img src="img/LPFS_logo.png" alt="Logo" class="logo">
-            <div class="nav-links">
-                <a href="admin.php">Accueil</a>
-                <a href="Pre_admission_Info.php">Pré-admission</a>
-            </div>
-        </nav>
-</header>
 <body>
+    <header class="navbar">
+        <div class="logo-container">
+            <img src="img/LPFS_logo.png" alt="Logo" class="logo">
+        </div>
+        <div class="page">
+            <a href="admin.php">Accueil</a>
+            <a href="Pre_admission_Info.php">Pré-admission</a>
+        </div>
+    </header>
     <div class="container-pre-admission">
         <form method="POST" action=""></form>
         <h6>INFORMATIONS CONCERNANT LE PATIENT</h6>
