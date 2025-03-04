119ac2ae6b49c4526b52e6722bcf9093778cff31
NoahMiMa <noah.mirandamatos@gmail.com>
2025-03-04 15:11:42 +0100

    Ajout page direction

---
+++ html/Direction.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<?php
+require('Logout.php');
+session_start();
+?>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page de connexion</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <header class="navbar">
+        <div class="logo-container">
+            <img src="img/LPFS_logo.png" alt="Logo" class="logo">
+        </div>
+        <div class="page">
+            <a href="admin.php">Accueil</a>
+            <a href="Pre_admission_Info.php">Pré-admission</a>        
+            <a href="?logout=true">Se déconnecter</a>
+        </div>
+    </header>
+</body>
+</html>
