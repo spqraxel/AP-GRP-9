63f07623d5bb099b07fc962f9ef96aedcfaffb40
KyllianCel <kylliancelisse.pro@gmail.com>
2025-03-04 16:03:52 +0100

    fix logs.php

--- html/logs.php
+++ html/logs.php
@@ -6,4 +6,10 @@ $motdepasse = "sio2425";
 $nomBDD = "AP_BTS2";
 $erreur = "";
 
+try {
+    $connexion = new PDO("mysql:host=$serveur;dbname=$nomBDD", $utilisateur, $motdepasse);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    $erreur = "Erreur de connexion : " . $e->getMessage();
+}
 ?>
