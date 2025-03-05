05b6c5d12e60f82cbce7ed44f3075aaa4317c109
KyllianCel <kylliancelisse.pro@gmail.com>
2025-03-05 17:30:20 +0100

    fixing pre add info

--- html/Pre_admission_Info.php
+++ html/Pre_admission_Info.php
@@ -37,6 +37,8 @@ function verifierCodePostalVille($cp, $ville) {
 
 // Traitement du formulaire
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $_SESSION['form_data'] = $_POST;
+
     // Récupération et nettoyage des données
     $num_secu = htmlspecialchars($_POST['num_secu']);
     $civilite = htmlspecialchars($_POST['civilite']);
@@ -56,15 +58,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (empty($num_secu) || strlen($num_secu) !== 15 || !ctype_digit($num_secu)) {
         $erreur = "Le numéro de sécurité sociale doit contenir exactement 15 chiffres.";
     } elseif (empty($nom_patient)) {
-        $erreur = "Le prénom est obligatoires.";
+        $erreur = "Le prénom est obligatoire.";
     } elseif (empty($prenom_patient)) {
-        $erreur = "Le nom est obligatoires.";
+        $erreur = "Le nom est obligatoire.";
     } elseif (empty($date_naissance) || strtotime($date_naissance) > time() || strtotime($date_naissance) < $limiteAgeMax) {
         $erreur = "La date de naissance n'est pas valide.";
     } elseif (!filter_var($email_patient, FILTER_VALIDATE_EMAIL)) {
         $erreur = "L'adresse e-mail n'est pas valide.";
     } elseif (empty($telephone_patient) || strlen($telephone_patient) !== 10 || !ctype_digit($telephone_patient)) {
-        $erreur = "Le numéro de telephone doit contenir 10 chiffres.";
+        $erreur = "Le numéro de téléphone doit contenir 10 chiffres.";
     } elseif (empty($CP) || strlen($CP) !== 5 || !ctype_digit($CP)) {
         $erreur = "Le code postal doit contenir 5 chiffres.";
     } elseif ($civilite !== "0" && $civilite !== "1") {
@@ -81,7 +83,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $numSecuSansCle = substr($num_secu, 0, -2);
         $cleCalculee = 97 - ($numSecuSansCle % 97);
 
-    
         if ($cle != $cleCalculee) {
             $erreur = "La clé de contrôle du numéro de sécurité sociale est incorrecte.";
         } else {
@@ -92,8 +93,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             } else {
                 try {
                     // Préparer la requête SQL
-                    $stmt = $connexion->prepare("INSERT INTO Patient (num_secu, nom_patient, prenom_patient, date_naissance, adresse, CP, ville, email_patient, telephone_patient, nom_epouse, civilite, id_pers1, id_pers2 ) 
-                    VALUES (:num_secu, :nom_patient, :prenom_patient, :date_naissance, :adresse, :CP, :ville, :email_patient, :telephone_patient, :nom_epouse, :civilite, 1, 1)");
+                    $stmt = $connexion->prepare("
+                        INSERT INTO Patient (
+                            num_secu, nom_patient, prenom_patient, date_naissance, adresse, CP, ville, 
+                            email_patient, telephone_patient, nom_epouse, civilite, id_pers1, id_pers2
+                        ) VALUES (
+                            :num_secu, :nom_patient, :prenom_patient, :date_naissance, :adresse, :CP, :ville, 
+                            :email_patient, :telephone_patient, :nom_epouse, :civilite, 1, 1
+                        )
+                    ");
 
                     // Lier les paramètres
                     $stmt->bindParam(':num_secu', $num_secu);
@@ -110,14 +118,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                     // Exécuter la requête
                     $stmt->execute();
+
+                    unset($_SESSION['form_data']);
+
                     header('Location: Pre_admission_Inscription.php');
                     exit();
                 } catch (PDOException $e) {
-                    echo "<p>Erreur lors de l'insertion : " . $e->getMessage() . "</p>";
+                    $erreur = "Erreur lors de l'insertion : " . $e->getMessage();
                 }
             }
         }
-    }   
+    }
 }
 
 // Activer l'affichage des erreurs
@@ -146,7 +157,6 @@ error_reporting(E_ALL);
         </div>
     </header>
     <div class="container-pre-admission">
-
         <!-- Formulaire -->
         <form method="POST" action="Pre_admission_Info.php">
             <h6>INFORMATIONS CONCERNANT LE PATIENT <br>Etape 1 sur 6</h6>
@@ -161,61 +171,61 @@ error_reporting(E_ALL);
 
             <label for="num_secu">Numéro de sécurité sociale :<span class= "requis"> *</span></label>
             <br>
-            <input type="text" id="num_secu" name="num_secu" required>
+            <input type="text" id="num_secu" name="num_secu" value="<?php echo htmlspecialchars($_SESSION['form_data']['num_secu'] ?? ''); ?>" required>
             <br><br>
 
             <label for="civilite">Civilité:<span class= "requis">*</span></label>
             <br>
             <select id="civilite" name="civilite" required>
                 <option value="-1" selected disabled hidden>choix</option>
-                <option value="1">Femme</option>
-                <option value="0">Homme</option>      
+                <option value="1" <?php echo (isset($_SESSION['form_data']['civilite']) && $_SESSION['form_data']['civilite'] == '1' ? 'selected' : ''); ?>>Femme</option>
+                <option value="0" <?php echo (isset($_SESSION['form_data']['civilite']) && $_SESSION['form_data']['civilite'] == '0' ? 'selected' : ''); ?>>Homme</option>      
             </select>
             <br><br>
 
             <label for="nom_patient">Nom de naissance :<span class= "requis"> *</span></label>
             <br>
-            <input type="text" id="nom_patient" name="nom_patient" required>
+            <input type="text" id="nom_patient" name="nom_patient" value="<?php echo htmlspecialchars($_SESSION['form_data']['nom_patient'] ?? ''); ?>" required>
             <br><br>
 
             <label for="nom_epouse">Nom d'épouse :</label>
             <br>
-            <input type="text" id="nom_epouse" name="nom_epouse">
+            <input type="text" id="nom_epouse" name="nom_epouse" value="<?php echo htmlspecialchars($_SESSION['form_data']['nom_epouse'] ?? ''); ?>">
             <br><br> 
 
             <label for="prenom_patient">Prénom :<span class= "requis"> *</span></label>
             <br>
-            <input type="text" id="prenom_patient" name="prenom_patient" required>
+            <input type="text" id="prenom_patient" name="prenom_patient" value="<?php echo htmlspecialchars($_SESSION['form_data']['prenom_patient'] ?? ''); ?>" required>
             <br><br>
 
             <label for="date_naissance">Date de naissance :<span class= "requis"> *</span></label>
             <br>
-            <input type="date" id="date_naissance" name="date_naissance" required>
+            <input type="date" id="date_naissance" name="date_naissance" value="<?php echo htmlspecialchars($_SESSION['form_data']['date_naissance'] ?? ''); ?>" required>
             <br><br>
 
             <label for="adresse">Adresse :<span class= "requis"> *</span></label>
             <br>
-            <input type="text" id="adresse" name="adresse" required>
+            <input type="text" id="adresse" name="adresse" value="<?php echo htmlspecialchars($_SESSION['form_data']['adresse'] ?? ''); ?>" required>
             <br><br>
 
             <label for="CP">CP :<span class= "requis"> *</span></label>
             <br>
-            <input type="text" id="CP" name="CP" required>
+            <input type="text" id="CP" name="CP" value="<?php echo htmlspecialchars($_SESSION['form_data']['CP'] ?? ''); ?>" required>
             <br><br>
 
             <label for="ville">Ville :<span class= "requis"> *</span></label>
             <br>
-            <input type="text" id="ville" name="ville" required>
+            <input type="text" id="ville" name="ville" value="<?php echo htmlspecialchars($_SESSION['form_data']['ville'] ?? ''); ?>" required>
             <br><br>
 
             <label for="email_patient">Email :<span class= "requis"> *</span></label>
             <br>
-            <input type="text" id="email_patient" name="email_patient" required>
+            <input type="text" id="email_patient" name="email_patient" value="<?php echo htmlspecialchars($_SESSION['form_data']['email_patient'] ?? ''); ?>" required>
             <br><br>
 
             <label for="telephone_patient">Téléphone :<span class= "requis">*</span></label>
             <br>
-            <input type="text" id="telephone_patient" name="telephone_patient" required>
+            <input type="text" id="telephone_patient" name="telephone_patient" value="<?php echo htmlspecialchars($_SESSION['form_data']['telephone_patient'] ?? ''); ?>" required>
             <br><br>
 
             <div class="navigation">
