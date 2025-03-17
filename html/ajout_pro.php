af9e24df83e60d2e0a75a3d6edc4245e571342fd
KyllianCel <kylliancelisse.pro@gmail.com>
2025-03-17 11:56:54 +0100

    modif logs.php

--- html/ajout_pro.php
+++ html/ajout_pro.php
@@ -1,7 +1,18 @@
 <?php
 require('Logout.php');
 
-require('logs.php');
+$serveur = "192.168.10.27:3306";
+$utilisateur = "dev";
+$motdepasse = "sio2425";
+$nomBDD = "AP_BTS2";
+$erreur = "";
+
+try {
+    $connexion = new PDO("mysql:host=$serveur;dbname=$nomBDD", $utilisateur, $motdepasse);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    $erreur = "Erreur de connexion : " . $e->getMessage();
+}
 
 $table = "professionnel"; // Modification pour correspondre à la table professionnel
 
--- html/modifier_pro.php
+++ html/modifier_pro.php
@@ -1,13 +1,17 @@
 <?php
 require('Logout.php');
 
-require('logs.php');
+$serveur = "192.168.10.27:3306";
+$utilisateur = "dev";
+$motdepasse = "sio2425";
+$nomBDD = "AP_BTS2";
+$erreur = "";
 
 try {
-    $connexion = new PDO("mysql:host=$serveur;dbname=$nomBDD", $utilisateur, $motDePasse);
+    $connexion = new PDO("mysql:host=$serveur;dbname=$nomBDD", $utilisateur, $motdepasse);
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
+    $erreur = "Erreur de connexion : " . $e->getMessage();
 }
 
 // Récupérer les métiers depuis la table "Metier"
