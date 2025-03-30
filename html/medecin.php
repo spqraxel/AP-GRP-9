1f54b5e90f5976ff53a1443c3f11d1c35b58920d
KyllianCel <kylliancelisse.pro@gmail.com>
2025-03-30 23:13:52 +0200

    fix doctors panel

--- html/medecin.php
+++ html/medecin.php
@@ -91,7 +91,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pré-admission</title>
+    <title>Panel Médecin</title>
     <link rel="stylesheet" href="style/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 </head>
