facbabcac0a4a60ad50565c7d6e818cb3583cf2b
KyllianCel <kylliancelisse.pro@gmail.com>
2025-03-30 20:53:06 +0200

    removing error function

--- html/Pre_admission_Inscription.php
+++ html/Pre_admission_Inscription.php
@@ -50,11 +50,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
-// Activer l'affichage des erreurs
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 ?>
 <!DOCTYPE html>
 <html lang="fr">
