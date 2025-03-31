ed10dfcc87ba45d50b3ec476fa4694349158b007
KyllianCel <kylliancelisse.pro@gmail.com>
2025-03-31 08:37:02 +0200

    fix

--- html/require/navbar.php
+++ html/require/navbar.php
@@ -10,14 +10,14 @@ $id_metier = $_SESSION['id_metier'] ?? null;
     <div class="page">
         <?php if ($id_metier == 2): ?>
             <!-- Navbar pour l'admin -->
-            <a href="secretaire.php">Accueil</a>
+            <a href="admin.php">Accueil</a>
             <a href="Pre_admission_Choix.php">Pré-admission</a>
             <a href="medecin.php">Médecin</a>
             <a href="?logout=true">Se déconnecter</a>
 
         <?php elseif ($id_metier == 1): ?>
             <!-- Navbar pour la secrétaire -->
-            <a href="admin.php">Accueil</a>
+            <a href="secretaire.php">Accueil</a>
             <a href="Pre_admission_Choix.php">Pré-admission</a>
             <a href="?logout=true">Se déconnecter</a>
 
